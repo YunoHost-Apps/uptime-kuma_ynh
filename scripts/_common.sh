1ab2ac6fe65e291302a3e2eca57074b17f72f21d
Hadrien <ketsapiwiq@protonmail.com>
2021-11-01 20:33:37 +0300

    fix pm2 install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="npm git pm2 nodejs"
+pkg_dependencies="npm git nodejs"
 
 #=================================================
 # PERSONAL HELPERS
